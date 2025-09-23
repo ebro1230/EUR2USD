da98abaffb54b7587db686b2b803a499df1a4e91
ebro1230 <ebro1230@gmail.com>
2025-09-23 18:05:11 -0400

    fixing intervals

--- app/api/get-rates/route.js
+++ app/api/get-rates/route.js
@@ -240,7 +240,7 @@ export async function GET() {
           (exchangeRate >= user.thresholdValue && trend.trend === "neutral") ||
           (exchangeRate >= user.thresholdValue && !user.trendNotifications)
         ) {
-          if (intervalDifference > user.interval) {
+          if (intervalDifference >= user.interval) {
             await User.findOneAndUpdate(
               { email: user.email },
               { $set: { lastCheck: currentTime } }
