0c055a50bccc8c813c4e686d1908011011de87f0
ebro1230 <ebro1230@gmail.com>
2025-09-23 19:37:44 -0400

    clean up console.logs

--- app/api/scraper-cheerio/route.js
+++ app/api/scraper-cheerio/route.js
@@ -7,19 +7,12 @@ export async function GET() {
     const { data } = await axios.get(
       "https://wise.com/gb/currency-converter/eur-to-usd-rate"
     );
-    // console.log("DATA");
-    // console.log(data);
+
     const $ = cheerio.load(data);
 
-    // let results = $("div._midMarketRateAmount_14arr_139")
-    //   .map((i, el) => $(el).text())
-    //   .get();
+    let results = $("#target-input").attr("value");
 
-    let results2 = $("#target-input").attr("value");
-
-    console.log("RESULTS 2: ", results2);
-
-    return NextResponse.json(results2, { status: 200 });
+    return NextResponse.json(results, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
--- app/api/scraper/route.js
+++ app/api/scraper/route.js
@@ -3,30 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
   try {
-    // Launch a headless browser
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
-    // Go to the target website
     await page.goto("https://wise.com/gb/currency-converter/eur-to-usd-rate", {
       waitUntil: "load",
     });
-
-    // Extract data from the page
-    // let results = await page.evaluate(() => {
-    //   return Array.from(
-    //     document.querySelectorAll("div._midMarketRateAmount_14arr_139")
-    //   ).map((q) => Number(q.innerText.substr(9, 5)));
-    // });
 
     const results2 = await page.evaluate(() => {
       return Array.from(
         document.querySelectorAll("h2.np-text-title-section.m-t-1")
       ).map((q) => Number(q.innerText.substr(8, 6)));
     });
-
-    //console.log("RESULTS: ", results);
-    //console.log("RESULTS 2: ", results2);
 
     await browser.close();
     return NextResponse.json(results2[0], { status: 200 });
