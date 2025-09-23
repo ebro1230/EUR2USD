f3402e593f354d494ea32bebae5917e1d6190bdd
ebro1230 <ebro1230@gmail.com>
2025-09-23 19:04:48 -0400

    troubleshooting stored data

--- app/api/get-rates/route.js
+++ app/api/get-rates/route.js
@@ -207,16 +207,22 @@ export async function GET() {
         Number(Number(rateData.replace(/,/g, "")) / 1000).toFixed(5)
       );
 
-      if (!exchangeRateTrendData.length) {
+      if (exchangeRateTrendData.length < 1) {
         exchangeRateTrendData = [exchangeRate];
+        console.log("stored rates < 1");
+        console.log(exchangeRateTrendData);
       } else if (
         exchangeRateTrendData.length <= 192 &&
         exchangeRateTrendData > 0
       ) {
         exchangeRateTrendData.push(exchangeRate);
+        console.log("stored rates > 0 & < 192");
+        console.log(exchangeRateTrendData);
       } else {
         exchangeRateTrendData = exchangeRateTrendData.slice(1);
         exchangeRateTrendData.push(exchangeRate);
+        console.log("stored rates > 192");
+        console.log(exchangeRateTrendData);
       }
 
       await ExchangeRateTrendData.findOneAndUpdate(
